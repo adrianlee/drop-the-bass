2807cac827e338cf1b04061881b20e7921748867
Adrian Lee <jun.irok@gmail.com>
2013-03-26 03:44:45 -0400

    Added play endpoint to sendfile(filePath)

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
-var fs = require('fs');
-  _ = require('lodash');
+var fs = require('fs'),
+  path = require('path'),
+  _ = require('lodash'),
+  express = require('express'),
+  app = express();
 
-var config = require('./config');
+var config = require('./config'),
+  music_path = config.dir || __dirname,
+  files;
 
-fs.readdir(config.dir || __dirname, function (err, files) {
-  files = _.filter(files, function(filename) {
+fs.readdir(music_path, function (err, filenames) {
+  files = _.filter(filenames, function(filename) {
     return filename.substr((~-filename.lastIndexOf(".") >>> 0) + 2) == "mp3";
   });
 
   console.log(files);
+
+  app.listen(3002);
+});
+
+app.get('/', function (req, res) {
+  res.send("<audio controls preload=\"auto\" autoplay autobuffer src=\"/play/" + files[0] + "\"></audio>");
+});
+
+app.get('/play/:filename', function (req, res) {
+  var filePath = path.join(music_path, req.param('filename'));
+  console.log(filePath);
+  res.sendfile(filePath);
 });
